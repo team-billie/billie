359239d81074340efea6648d8a405689b917481a
5almiakki <jpui9635@gmail.com>
2025-04-29 17:49:49 +0900

    :wrench: [chore] 초기 프로젝트 생성 #S12P31E205-92

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/NextdoorApplication.java
@@ -0,0 +1,13 @@
+package com.nextdoor.nextdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextdoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextdoorApplication.class, args);
+	}
+
+}
