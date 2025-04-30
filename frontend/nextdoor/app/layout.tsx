cd4c693266c6c27b2e9e34381e112663c3a27ba6
dahee1029 <dlaekgml222@naver.com>
2025-04-30 11:41:36 +0900

    :wrench:[chore] 초기 프로젝트 생성#S12P31E205-72

---
+++ frontend/nextdoor/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | 옆집물건",
+    default: "옆집물건",
+  },
+  description: "사용해보고 싶었던 모든 물건, 옆집물건에서 빌려보세요!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
