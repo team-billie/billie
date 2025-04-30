c78a629b34c20b14f5446c2d886af176b02f4684
dahee1029 <dlaekgml222@naver.com>
2025-04-30 16:59:20 +0900

    :wrench:[chore] DasiyUi 패키지 추가 #S12P31E205-72

--- frontend/nextdoor/tailwind.config.ts
+++ frontend/nextdoor/tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -14,6 +16,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  //base layer와 utilities layerㅇ와 components layer를 확장하는 역할
+  plugins: [forms, daisyui],
 };
 export default config;
