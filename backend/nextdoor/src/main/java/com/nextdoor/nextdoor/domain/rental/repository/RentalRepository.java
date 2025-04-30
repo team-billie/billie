13e9b13723efb45036731e841242d68e5ba187e5
Choijake <shameless8@naver.com>
2025-04-30 21:56:00 +0900

    :sparkles: [feat] registerBeforePhoto에 s3 이미지 업로드 로직 통합 #S12P31E205-89

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/repository/RentalRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/repository/RentalRepository.java
@@ -4,6 +4,9 @@ import com.nextdoor.nextdoor.domain.rental.domain.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    Optional<Rental> findByRentalId(Long rentalId);
 }
--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/service/RentalServiceImpl.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/service/RentalServiceImpl.java
@@ -3,6 +3,7 @@ package com.nextdoor.nextdoor.domain.rental.service;
 import com.nextdoor.nextdoor.domain.rental.domain.Rental;
 import com.nextdoor.nextdoor.domain.rental.event.UploadImageEvent;
 import com.nextdoor.nextdoor.domain.rental.repository.RentalRepository;
+import com.nextdoor.nextdoor.domain.rental.service.dto.S3UploadResult;
 import com.nextdoor.nextdoor.domain.rental.service.dto.UploadBeforeImageCommand;
 import com.nextdoor.nextdoor.domain.rental.service.dto.UploadBeforeImageResult;
 import lombok.RequiredArgsConstructor;
@@ -10,19 +11,27 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class RentalServiceImpl implements RentalService {
 
     private final RentalRepository rentalRepository;
+    private final S3ImageUploadService s3ImageUploadService;
     private final ApplicationEventPublisher eventPublisher;
 
     @Override
     @Transactional
     public UploadBeforeImageResult registerBeforePhoto(UploadBeforeImageCommand command) {
+        Rental rental = this.rentalRepository.findByRentalId(command.getRentalId())
+                .orElseThrow(() -> new IllegalArgumentException("대여 정보가 존재하지 않습니다."));
 
+        //TODO : s3 업로드 구현체, 업로드 예외 처리
+        S3UploadResult imageUploadResult = s3ImageUploadService.upload(command.getFile(), "rentals/" + rental.getRentalId() + "/before");
+        rental.saveAiImage(imageUploadResult.getUrl(), command.getFile().getContentType());
+        LocalDateTime uploadedAt = LocalDateTime.now();
 
-        eventPublisher.publishEvent(new UploadImageEvent(command.getFile()));
-        return null;
+        return new UploadBeforeImageResult(rental.getRentalId(), imageUploadResult.getUrl(), uploadedAt);
     }
 }
