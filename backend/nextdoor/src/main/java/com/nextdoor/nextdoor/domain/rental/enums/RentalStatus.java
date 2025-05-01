6cfe645f049c345d26f3da74642546cb68eae321
Choijake <shameless8@naver.com>
2025-05-02 01:15:16 +0900

    :sparkles: [feat] REMITTANCE_CONFIRMED, AFTER_PHOTO_REGISTERED 상태 추가 #S12P31E205-89

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
@@ -3,6 +3,8 @@ package com.nextdoor.nextdoor.domain.rental.enums;
 public enum  RentalStatus {
     CREATED,
     REMITTANCE_REQUESTED,
+    REMITTANCE_CONFIRMED,
+    AFTER_PHOTO_REGISTERED,
     ACTIVE,
     COMPLETED,
     CANCELLED
