6a7a59ffd72f29f067ff69c6fd31dc38243deffc
임다희 <dlaekgml222@naver.com>
2025-05-01 18:23:04 +0900

    :art:[style] AI 물품 사진 관리  페이지 레이아웃 #S12P31E205-76

--- frontend/nextdoor/app/reservations/[id]/safe-deal/manage/page.tsx
+++ frontend/nextdoor/app/reservations/[id]/safe-deal/manage/page.tsx
@@ -1,7 +1,17 @@
+import PhotoManager from "@/components/reservations/safe-deal/manage/PhotoManager";
+
 export default function SafeDealManage() {
   return (
     <main>
-      <div className="w-full h-screen bg-slate-500">dsf</div>
+      <div className="h-screen flex flex-col p-4">
+        <div>
+          <PhotoManager status="대여 물품 사진" />
+          <PhotoManager status="반납 물품 사진" />
+          <button className="btn w-full mx-2 h-14 bg-gradient-to-r from-blue200 to-blue400 text-white">
+            AI 분석 결과 확인
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
