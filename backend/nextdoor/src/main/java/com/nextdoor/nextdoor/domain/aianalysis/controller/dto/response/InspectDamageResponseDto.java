1d3154530ab3ccbbff81fffa05f96641077b4651
5almiakki <jpui9635@gmail.com>
2025-05-02 00:40:18 +0900

    :sparkles: [feat] 검사 응답 DTO 추가 #S12P31E205-73

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/controller/dto/response/InspectDamageResponseDto.java
@@ -0,0 +1,11 @@
+package com.nextdoor.nextdoor.domain.aianalysis.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class InspectDamageResponseDto {
+
+    private String result;
+}
