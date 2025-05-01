2e17c8900d527de7725c4c902fb88be6b9af57a2
5almiakki <jpui9635@gmail.com>
2025-05-02 00:42:51 +0900

    :sparkles: [feat] Gemini 서비스 추가 #S12P31E205-73

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/GeminiAnalysisService.java
@@ -0,0 +1,57 @@
+package com.nextdoor.nextdoor.domain.aianalysis.service;
+
+import com.google.cloud.vertexai.api.Content;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.api.Part;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.PartMaker;
+import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.request.InspectDamageRequestDto;
+import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.response.InspectDamageResponseDto;
+import com.nextdoor.nextdoor.domain.aianalysis.exception.ExternalApiException;
+import com.nextdoor.nextdoor.domain.aianalysis.service.dto.AiImageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class GeminiAnalysisService implements AiAnalysisService {
+
+    private final GenerativeModel generativeModel;
+    private final Part promptPart;
+
+    private final AiAnalysisRentalQueryService aiAnalysisRentalQueryService;
+
+    @Override
+    public InspectDamageResponseDto inspectDamage(InspectDamageRequestDto inspectDamageRequestDto) {
+        List<AiImageDto> aiImages = aiAnalysisRentalQueryService.findByRentalId(inspectDamageRequestDto.getRentalId());
+        GenerateContentResponse response;
+        try {
+            response = generativeModel.generateContent(createContent(aiImages));
+        } catch (IOException e) {
+            throw new ExternalApiException(e);
+        }
+        return new InspectDamageResponseDto(response.getCandidates(0).getContent().getParts(0).getText());
+    }
+
+    private Content createContent(List<AiImageDto> aiImages) {
+        List<Part> beforeImageParts = aiImages.stream()
+                .filter(aiImageDto -> aiImageDto.getType().equals(AiImageDto.Type.BEFORE))
+                .map(aiImageDto -> PartMaker.fromMimeTypeAndData(aiImageDto.getMimeType(), aiImageDto.getImageUrl()))
+                .toList();
+        List<Part> afterImageParts = aiImages.stream()
+                .filter(aiImageDto -> aiImageDto.getType().equals(AiImageDto.Type.AFTER))
+                .map(aiImageDto -> PartMaker.fromMimeTypeAndData(aiImageDto.getMimeType(), aiImageDto.getImageUrl()))
+                .toList();
+        return Content.newBuilder()
+                .addAllParts(beforeImageParts)
+                .addParts(Part.newBuilder().setText("These are before images.").build())
+                .addAllParts(afterImageParts)
+                .addParts(Part.newBuilder().setText("These are after images.").build())
+                .addParts(promptPart)
+                .setRole("user")
+                .build();
+    }
+}
