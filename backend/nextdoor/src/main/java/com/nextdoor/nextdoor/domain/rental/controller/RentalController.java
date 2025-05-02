5ac13b17821401a7e69a02920afc0a3bfe2f5019
Choijake <shameless8@naver.com>
2025-05-02 17:48:59 +0900

    :sparkles: [feat] 대여 목록 조회 api 작성 중 #S12P31E205-89

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/controller/RentalController.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/controller/RentalController.java
@@ -1,15 +1,19 @@
 package com.nextdoor.nextdoor.domain.rental.controller;
 
 import com.nextdoor.nextdoor.domain.rental.controller.dto.request.RequestRemittanceRequest;
+import com.nextdoor.nextdoor.domain.rental.controller.dto.request.RetrieveRentalsRequest;
 import com.nextdoor.nextdoor.domain.rental.controller.dto.request.UploadImageRequest;
+import com.nextdoor.nextdoor.domain.rental.controller.dto.response.RentalDetailResponse;
 import com.nextdoor.nextdoor.domain.rental.controller.dto.response.UploadImageResponse;
 import com.nextdoor.nextdoor.domain.rental.mapper.RentalMapper;
 import com.nextdoor.nextdoor.domain.rental.service.RentalService;
-import com.nextdoor.nextdoor.domain.rental.service.dto.RequestRemittanceCommand;
-import com.nextdoor.nextdoor.domain.rental.service.dto.UploadImageCommand;
-import com.nextdoor.nextdoor.domain.rental.service.dto.UploadImageResult;
+import com.nextdoor.nextdoor.domain.rental.service.dto.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +24,21 @@ public class RentalController {
 
     private final RentalService rentalService;
     private final RentalMapper rentalMapper;
+
+    @GetMapping
+    public ResponseEntity<Page<RentalDetailResponse>> getMyRentals(
+            Long userId,
+            @ModelAttribute RetrieveRentalsRequest retrieveRentalsRequest,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        SearchRentalCommand command = rentalMapper.toCommand(userId, retrieveRentalsRequest, pageable);
+        Page<SearchRentalResult> results = rentalService.searchRentals(command);
+        Page<RentalDetailResponse> responsePage =
+                results.map(rentalMapper::toResponse);
+
+        return ResponseEntity.ok(responsePage);
+    }
 
     @PostMapping("/{rentalId}/before/photos")
     ResponseEntity<UploadImageResponse> registerBeforePhoto(@PathVariable Long rentalId,
