c4090446eb550a3637a95bda4d02ac6151d6d095
Choijake <shameless8@naver.com>
2025-05-02 11:11:33 +0900

    :sparkles: [feat] BEFORE_PHOTO_REGISTERED 상태 추가 #S12P31E205-89

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
@@ -2,6 +2,7 @@ package com.nextdoor.nextdoor.domain.rental.enums;
 
 public enum  RentalStatus {
     CREATED,
+    BEFORE_PHOTO_REGISTERED,
     REMITTANCE_REQUESTED,
     REMITTANCE_CONFIRMED,
     AFTER_PHOTO_REGISTERED,
