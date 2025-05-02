10356f1589c52c6e12efc09c8eaa6885aafd9ceb
dahee1029 <dlaekgml222@naver.com>
2025-05-02 12:37:09 +0900

    :art:[style] 예약 현황 페이지 헤더 & 하단 메뉴

---
+++ frontend/nextdoor/app/reservations/manage/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import MainDock from "@/components/common/Dock/MainDock";
+import MainHeader from "@/components/common/Header/ReservationHeader";
+import ReservationStatusTabs from "@/components/reservations/safe-deal/overview/ReservationStatusTabs";
+
+// import PhotoNotFound from "@/components/reservations/safe-deal/PhotoNotFound";
+
+export default function ReservationMangePage() {
+  return (
+    <main>
+      <MainHeader title="Reservations" />
+      <ReservationStatusTabs />
+      <div className="h-screen flex flex-col p-4"></div>
+      <MainDock />
+    </main>
+  );
+}
