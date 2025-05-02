aaf808fb84005039987ad6d99ea5fb78d7aa5e9f
Choijake <shameless8@naver.com>
2025-05-02 17:43:39 +0900

    :sparkles: [feat] 대여 목록 조회 서비스 DTO 및 매퍼 추가 #S12P31E205-89

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/mapper/RentalMapper.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/mapper/RentalMapper.java
@@ -1,11 +1,12 @@
 package com.nextdoor.nextdoor.domain.rental.mapper;
 
 import com.nextdoor.nextdoor.domain.rental.controller.dto.request.RequestRemittanceRequest;
+import com.nextdoor.nextdoor.domain.rental.controller.dto.request.RetrieveRentalsRequest;
 import com.nextdoor.nextdoor.domain.rental.controller.dto.request.UploadImageRequest;
+import com.nextdoor.nextdoor.domain.rental.controller.dto.response.RentalDetailResponse;
 import com.nextdoor.nextdoor.domain.rental.controller.dto.response.UploadImageResponse;
-import com.nextdoor.nextdoor.domain.rental.service.dto.RequestRemittanceCommand;
-import com.nextdoor.nextdoor.domain.rental.service.dto.UploadImageCommand;
-import com.nextdoor.nextdoor.domain.rental.service.dto.UploadImageResult;
+import com.nextdoor.nextdoor.domain.rental.service.dto.*;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -32,6 +33,34 @@ public class RentalMapper {
                 request.getRenterId(),
                 request.getRemittanceAmount()
         );
+    }
+
+    public SearchRentalCommand toCommand(Long userId,
+                                               RetrieveRentalsRequest request,
+                                               Pageable pageable) {
+        return SearchRentalCommand.builder()
+                .userId(userId)
+                .userRole(request.getUserRole())
+                .condition(request.getCondition())
+                .pageable(pageable)
+                .build();
+    }
+
+    public RentalDetailResponse toResponse(SearchRentalResult result) {
+        return RentalDetailResponse.builder()
+                .reservationId(result.getReservationId())
+                .startDate(result.getStartDate())
+                .endDate(result.getEndDate())
+                .rentalFee(result.getRentalFee())
+                .deposit(result.getDeposit())
+                .reservationStatus(result.getReservationStatus())
+                .ownerId(result.getOwnerId())
+                .renterId(result.getRenterId())
+                .rentalId(result.getRentalId())
+                .rentalStatus(result.getRentalStatus())
+                .title(result.getTitle())
+                .productImageUrl(result.getProductImageUrl())
+                .build();
     }
 }
 
