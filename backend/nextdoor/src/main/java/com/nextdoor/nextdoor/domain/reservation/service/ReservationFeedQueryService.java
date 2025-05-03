8f9d1c0324908dfb964e08a4194a70f81f0e367d
5almiakki <jpui9635@gmail.com>
2025-05-03 16:21:29 +0900

    :sparkles: [feat] 대여 도메인에서 피드 조회 service 추가 #S12P31E205-96

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationFeedQueryService.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationFeedQueryService.java
@@ -1,6 +1,8 @@
 package com.nextdoor.nextdoor.domain.reservation.service;
 
+import com.nextdoor.nextdoor.domain.reservation.service.dto.FeedDto;
+
 public interface ReservationFeedQueryService {
 
-
+    FeedDto findById(Long feedId);
 }
