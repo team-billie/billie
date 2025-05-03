7ad82a52007862c6b6e0b3bcba3186c8cdc66fe6
5almiakki <jpui9635@gmail.com>
2025-05-03 22:44:19 +0900

    :sparkles: [feat] 예약 수정 API 추가 #S12P31E205-96

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/controller/ReservationController.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/controller/ReservationController.java
@@ -1,16 +1,14 @@
 package com.nextdoor.nextdoor.domain.reservation.controller;
 
 import com.nextdoor.nextdoor.domain.reservation.controller.dto.request.ReservationSaveRequestDto;
+import com.nextdoor.nextdoor.domain.reservation.controller.dto.request.ReservationUpdateRequestDto;
 import com.nextdoor.nextdoor.domain.reservation.controller.dto.response.ReservationResponseDto;
 import com.nextdoor.nextdoor.domain.reservation.service.ReservationService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -29,6 +27,17 @@ public class ReservationController {
     ) {
         Long loginUserId = 1L;
         ReservationResponseDto reservationResponseDto = reservationService.createReservation(loginUserId, reservationSaveRequestDto);
+        return ResponseEntity.created(URI.create(request.getRequestURI())).body(reservationResponseDto);
+    }
+
+    @PutMapping("/{reservationId}")
+    public ResponseEntity<ReservationResponseDto> updateReservation(
+            HttpServletRequest request,
+            @PathVariable Long reservationId,
+            @RequestBody ReservationUpdateRequestDto reservationUpdateRequestDto
+    ) {
+        Long loginUserId = 1L;
+        ReservationResponseDto reservationResponseDto = reservationService.updateReservation(loginUserId, reservationId, reservationUpdateRequestDto);
         return ResponseEntity.created(URI.create(request.getRequestURI())).body(reservationResponseDto);
     }
 }
