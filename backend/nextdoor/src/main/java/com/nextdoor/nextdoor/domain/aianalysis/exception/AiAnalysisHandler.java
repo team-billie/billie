cce03f3d8b85f4e46790a0f57aaeeae5bd0555c1
5almiakki <jpui9635@gmail.com>
2025-05-05 14:24:29 +0900

    :sparkles: [feat] 타 도메인 예외 핸들러 대상 패키지 수정 #S12P31E205-96

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/exception/AiAnalysisHandler.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/exception/AiAnalysisHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.nextdoor.nextdoor.domain.aianalysis")
 public class AiAnalysisHandler {
 
     @ExceptionHandler({
