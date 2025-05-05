9c01c93c6fb884e4630eafa2e9b3a41785fcd1b9
5almiakki <jpui9635@gmail.com>
2025-05-05 15:31:06 +0900

    :recycle: [refactor] AI 도메인 예외 핸들러 리펙토링
    - 로그 출력 및 예외 처리 로직 별도 메서드로 분리

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/exception/AiAnalysisHandler.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/exception/AiAnalysisHandler.java
@@ -2,6 +2,7 @@ package com.nextdoor.nextdoor.domain.aianalysis.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,34 +15,42 @@ public class AiAnalysisHandler {
             DamageAnalysisPresentException.class
     })
     public ResponseEntity<ErrorResponseDto> handleDamageAnalysisException(DamageAnalysisPresentException e, HttpServletRequest request) {
-        log.error(e.getMessage(), e);
-        return ResponseEntity.badRequest().body(new ErrorResponseDto(
-                e.getErrorCode(),
-                e.getMessage(),
-                request.getRequestURI()
-        ));
+        return logAndHandleException(HttpStatus.BAD_REQUEST, e, request);
     }
 
     @ExceptionHandler({
             ExternalApiException.class
     })
     public ResponseEntity<ErrorResponseDto> handleExternalApiException(ExternalApiException e, HttpServletRequest request) {
-        log.error(e.getMessage(), e);
-        return ResponseEntity.internalServerError().body(new ErrorResponseDto(
-                e.getErrorCode(),
-                e.getMessage(),
-                request.getRequestURI()
-        ));
+        return logAndHandleException(HttpStatus.INTERNAL_SERVER_ERROR, e, request);
     }
 
     @ExceptionHandler({
             Exception.class
     })
     public ResponseEntity<ErrorResponseDto> handleException(Exception e, HttpServletRequest request) {
+        return logAndHandleException(HttpStatus.INTERNAL_SERVER_ERROR, e, "UNKNOWN", "알 수 없는 에러", request);
+    }
+
+    private ResponseEntity<ErrorResponseDto> logAndHandleException(
+            HttpStatus httpStatus,
+            BaseCustomException e,
+            HttpServletRequest request
+    ) {
+        return logAndHandleException(httpStatus, e, e.getErrorCode(), e.getMessage(), request);
+    }
+
+    private ResponseEntity<ErrorResponseDto> logAndHandleException(
+            HttpStatus httpStatus,
+            Exception e,
+            String errorCode,
+            String message,
+            HttpServletRequest request
+    ) {
         log.error(e.getMessage(), e);
-        return ResponseEntity.internalServerError().body(new ErrorResponseDto(
-                "UNKNOWN",
-                "알 수 없는 에러",
+        return ResponseEntity.status(httpStatus).body(new ErrorResponseDto(
+                errorCode,
+                message,
                 request.getRequestURI()
         ));
     }
