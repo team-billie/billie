aa99d205d2f8158bd4625ceb8a426324a3c9ec44
Choijake <shameless8@naver.com>
2025-05-05 23:37:17 +0900

    :sparkles: [feat] 결제 완료 이벤트의 후속 잡업을 처리하는 RemittanceCompletedEventListener 추가 #S12P31E205-89

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/listener/RemittanceCompletedEventListener.java
@@ -0,0 +1,30 @@
+package com.nextdoor.nextdoor.domain.rental.listener;
+
+import com.nextdoor.nextdoor.domain.rental.enums.RentalProcess;
+import com.nextdoor.nextdoor.domain.rental.enums.RentalStatus;
+import com.nextdoor.nextdoor.domain.rental.event.in.RemittanceCompletedEvent;
+import com.nextdoor.nextdoor.domain.rental.message.RentalStatusMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class RemittanceCompletedEventListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Async("asyncExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleRemittanceCompletedEvent(RemittanceCompletedEvent remittanceCompletedEvent){
+        messagingTemplate.convertAndSend("/topic/rental" + remittanceCompletedEvent.getRenterId() + "/status",
+                RentalStatusMessage.builder()
+                        .process(RentalProcess.RENTAL_IN_PROGRESS.name())
+                        .detailStatus(RentalStatus.REMITTANCE_CONFIRMED.name())
+                        .build()
+        );
+    }
+}
