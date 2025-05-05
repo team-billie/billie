3584112a4a7bb724c5f08f3bbd6c1f094606e666
안다정 <dksekwjd312@naver.com>
2025-05-05 23:59:44 +0900

    :sparkles: [feat] Profile : LinkHeader, PayBox 컴포넌트 제작

---
+++ frontend/nextdoor/components/(tabs)/profile/LinkHeader/index.tsx
@@ -0,0 +1,40 @@
+import { DollarSign, Heart, Calendar} from "lucide-react";   
+
+type HeaderType = keyof typeof HEADER_DATA;
+
+interface LinkHeaderProps {
+  type: HeaderType;
+}
+
+const HEADER_DATA = {
+  account: {
+    icon: DollarSign,
+    label: "계좌관리",
+  },
+  like: {
+    icon: Heart,
+    label: "관심물품",
+  },
+  reservation: {
+    icon: Calendar,
+    label: "예약관리",
+  },
+  apply: {
+    icon: Heart,
+    label: "요청관리",
+  },
+} as const;
+
+
+export default function LinkHeader ({type}: LinkHeaderProps){
+  const { icon: Icon, label } = HEADER_DATA[type];
+
+  return(
+    <div className="flex items-center gap-2">
+      <div className="flex justify-center items-center bg-blue200 bg-opacity-40 w-8 h-8 rounded-lg">
+        <Icon className="text-blue300 w-5" />
+      </div>
+      <span className="text-gray-700 font-medium">{label}</span>
+    </div>
+  )
+}
