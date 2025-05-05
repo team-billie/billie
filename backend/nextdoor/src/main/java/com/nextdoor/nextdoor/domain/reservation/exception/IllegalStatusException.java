f89abf5c50cfdbf15a389fa63df0fbabb16f1b50
5almiakki <jpui9635@gmail.com>
2025-05-05 14:27:12 +0900

    :sparkles: [feat] 예약 상태 변경 파라미터 예외 추가 #S12P31E205-96

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/exception/IllegalStatusException.java
@@ -0,0 +1,27 @@
+package com.nextdoor.nextdoor.domain.reservation.exception;
+
+public class IllegalStatusException extends BaseCustomException {
+
+    private final String errorCode = "ILLEGAL_STATUS";
+
+    public IllegalStatusException() {
+        super();
+    }
+
+    public IllegalStatusException(String message) {
+        super(message);
+    }
+
+    public IllegalStatusException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalStatusException(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
