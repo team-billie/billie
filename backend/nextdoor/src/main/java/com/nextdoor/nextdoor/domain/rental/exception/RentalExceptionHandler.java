7c6a84051fcccb2324eed8539d01d55afb0bf459
Choijake <shameless8@naver.com>
2025-05-06 01:35:34 +0900

    :sparkles: [feat] 대여 전역 예외 처리기 추가 #S12P31E205-89

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/exception/RentalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.nextdoor.nextdoor.domain.rental.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.nextdoor.nextdoor.domain.rental")
+public class RentalExceptionHandler {
+
+    @ExceptionHandler({
+            NoSuchRentalException.class,
+            InvalidRentalStatusException.class,
+            RentalImageUploadException.class,
+            RentalScheduleException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BaseCustomException e, HttpServletRequest request) {
+        return logAndHandleException(HttpStatus.BAD_REQUEST, e, request);
+    }
+
+    @ExceptionHandler({
+            HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
+        return logAndHandleException(HttpStatus.BAD_REQUEST, e, "ILLEGAL_REQUEST", "잘못된 요청입니다.", request);
+    }
+
+    @ExceptionHandler({
+            Exception.class
+    })
+    public ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
+        return logAndHandleException(HttpStatus.INTERNAL_SERVER_ERROR, e, "UNKNOWN", "알 수 없는 에러", request);
+    }
+
+    private ResponseEntity<ErrorResponse> logAndHandleException(
+            HttpStatus httpStatus,
+            BaseCustomException e,
+            HttpServletRequest request
+    ) {
+        return logAndHandleException(httpStatus, e, e.getErrorCode(), e.getMessage(), request);
+    }
+
+    private ResponseEntity<ErrorResponse> logAndHandleException(
+            HttpStatus httpStatus,
+            Exception e,
+            String errorCode,
+            String message,
+            HttpServletRequest request
+    ) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(httpStatus).body(new ErrorResponse(
+                errorCode,
+                message,
+                request.getRequestURI()
+        ));
+    }
+}
