1882f7cbcc78aa697ef704369f2b4a777d68cebb
허준수 <gjwnstn515@naver.com>
2025-05-06 18:39:18 +0900

    :sparkles: [feat] FintechUser 엔티티 추가 #S12P31E205-93

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/domain/FintechUser.java
@@ -0,0 +1,24 @@
+package com.nextdoor.nextdoor.domain.fintech.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "fintech_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FintechUser {
+    @Id
+    private String userKey;
+    private String email;
+    private LocalDateTime createdAt;
+}
