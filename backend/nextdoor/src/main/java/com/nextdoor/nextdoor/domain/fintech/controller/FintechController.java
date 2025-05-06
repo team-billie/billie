f680162a0d019ca4494ec08acd92987e8725d356
허준수 <gjwnstn515@naver.com>
2025-05-06 19:17:15 +0900

    :sparkles: [feat] 계정 생성 컨트롤러 추가 #S12P31E205-93

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/controller/FintechController.java
@@ -0,0 +1,27 @@
+package com.nextdoor.nextdoor.domain.fintech.controller;
+
+import com.nextdoor.nextdoor.domain.fintech.dto.*;
+import com.nextdoor.nextdoor.domain.fintech.domain.*;
+import com.nextdoor.nextdoor.domain.fintech.service.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/fintech")
+@RequiredArgsConstructor
+public class FintechController {
+    private final FintechUserService userService;
+    private final AccountService accountService;
+    private final DepositService depositService;
+
+    //계정 생성
+    @PostMapping("/users")
+    public Mono<ResponseEntity<FintechUser>> createUser(@RequestBody CreateUserRequestDto req) {
+        return userService.createUser(req.getEmail(), req.getApiKey())
+                .map(ResponseEntity::ok);
+    }
+
+
+}
