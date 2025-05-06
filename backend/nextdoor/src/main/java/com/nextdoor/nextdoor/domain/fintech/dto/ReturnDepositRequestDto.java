0f3a2e2ef8434c4899391676739ebc73f6cd7977
허준수 <gjwnstn515@naver.com>
2025-05-07 04:09:20 +0900

    :recycle: [refactor] 보증금 반환 Dto 수정 #S12P31E205-93

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/ReturnDepositRequestDto.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/ReturnDepositRequestDto.java
@@ -2,9 +2,9 @@ package com.nextdoor.nextdoor.domain.fintech.dto;
 
 import lombok.Data;
 
+// 보증금 반환 Dto
 @Data
 public class ReturnDepositRequestDto {
     private String userKey;
-    private String apiKey;
     private Long depositId;
 }
