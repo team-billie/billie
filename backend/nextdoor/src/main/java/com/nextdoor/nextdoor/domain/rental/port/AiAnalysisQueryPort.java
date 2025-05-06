edce647dbe9cafe03496dd41262aa16cd6915f11
Choijake <shameless8@naver.com>
2025-05-06 23:43:41 +0900

    :sparkles: [feat] 클래스 명 ~port로 변경 및 어노테이션 지정 #S12P31E205-99

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/port/AiAnalysisQueryPort.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/port/AiAnalysisQueryPort.java
@@ -1,9 +1,9 @@
 package com.nextdoor.nextdoor.domain.rental.port;
 
+import com.nextdoor.nextdoor.common.OutboundPort;
 import com.nextdoor.nextdoor.domain.rental.service.dto.AiAnalysisResult;
-import org.springframework.stereotype.Repository;
 
-@Repository
+@OutboundPort
 public interface AiAnalysisQueryPort {
     AiAnalysisResult getAiAnalysisResult(Long rentalId);
 }
