c73689f46ba4fc38635f89c983fcc7ab331c5b34
Choijake <shameless8@naver.com>
2025-05-06 23:41:59 +0900

    :sparkles: [feat] RENTAL_IN_ACTIVE로 대여 중 렌탈 프로세스 수정  #S12P31E205-99

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/enums/RentalStatus.java
@@ -1,13 +1,13 @@
 package com.nextdoor.nextdoor.domain.rental.enums;
 
 public enum RentalStatus {
-    CREATED, //안심 사진 등록 전
-    BEFORE_PHOTO_REGISTERED, //안심 사진 등록 후
-    REMITTANCE_REQUESTED, //결제 요청 상태
+    CREATED,
+    BEFORE_PHOTO_REGISTERED,
+    REMITTANCE_REQUESTED,
     REMITTANCE_CONFIRMED,
-    RENTAL_PERIOD_ENDED,//결제 확인 -> 대여 시작 -> 클라이언트에게 어떻게 알려주지?
-    AFTER_PHOTO_REGISTERED,//대여 당일 되면 클라이언트에게 사진 등록하라고 알려줘야함,
+    RENTAL_PERIOD_ENDED,
+    AFTER_PHOTO_REGISTERED,
     DEPOSIT_REQUESTED,
-    RENTAL_COMPLETED, //보증금 0원이면 바로 COMPLETED, 보증금 있으면 OWNER 보증금 처리 후 COMPLETED
+    RENTAL_COMPLETED,
     CANCELLED
 }
