105012ccd2a387fe3785822abd1f3180fd556e00
허준수 <gjwnstn515@naver.com>
2025-05-06 18:09:22 +0900

    :hammer_and_wrench: [config] build.gradle에 webflux 추가 #S12P31E205-93

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/client/SsafyApiClient.java
@@ -0,0 +1,10 @@
+package com.nextdoor.nextdoor.domain.fintech.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SsafyApiClient {
+
+}
