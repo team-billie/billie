1728f2c29847a9b72c82e448eebad908cfb31bea
허준수 <gjwnstn515@naver.com>
2025-05-06 18:12:13 +0900

    :sparkles: [feat] WebClient builder 추가 #S12P31E205-93

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/client/SsafyApiClient.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/client/SsafyApiClient.java
@@ -4,7 +4,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Component
 public class SsafyApiClient {
-
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("https://finopenapi.ssafy.io/ssafy/api/v1")
+            .defaultHeader("Content-Type", "application/json")
+            .build();
 }
