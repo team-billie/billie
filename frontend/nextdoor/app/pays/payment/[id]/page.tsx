1a9ba830b6d2647e9cc84dfac660eeb6dedfc4cf
안다정 <dksekwjd312@naver.com>
2025-05-07 15:13:26 +0900

    :sparkles: [feat] Pay : Loading 컴포넌트 제작 #S12P31E205-84

--- frontend/nextdoor/app/pays/payment/[id]/page.tsx
+++ frontend/nextdoor/app/pays/payment/[id]/page.tsx
@@ -3,20 +3,25 @@
 import Header from "@/components/pays/common/Header";
 import Button from "@/components/pays/common/Button";
 import { useState } from "react";
+import { Check } from "lucide-react";
 import AutoRechargeModal from "@/components/pays/modals/AutoRechargeModal";
+import Loading from "@/components/pays/common/Loading";
 
 export default function PaymentPage() {
     const [autoRecharge, setAutoRecharge] = useState(true);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isSuccess, setIsSuccess] = useState(true);
 
-    const submitBtnHandler = () => {
+    const sendBtnHandler = () => {
         if (autoRecharge) { setIsModalOpen(true) }
-
     }
-    
+
     return (
-    <div className="flex flex-col min-h-screen">
-        <Header txt="빌리페이 송금" />
+    <div className="relative flex flex-col min-h-screen">
+        {isLoading 
+        ?<Loading txt="결제가" isSuccess={isSuccess} headerTxt="빌리페이 송금"/>
+        :<><Header txt="빌리페이 송금" />
         <div className="flex-1 flex flex-col items-center p-6">
             <div className="w-full flex-1 flex flex-col items-center gap-3 pt-44">
                 <div className="flex items-center gap-2 mb-6">
@@ -29,12 +34,15 @@ export default function PaymentPage() {
                 </div>
             </div>
 
-            <Button txt="보내기" state={true} onClick={submitBtnHandler}/>
+            <Button txt="보내기" state={true} onClick={sendBtnHandler}/>
         </div>
+        </>
+        }
+        
         {isModalOpen && 
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60">
-            <AutoRechargeModal setIsModalOpen={setIsModalOpen}/>
-        </div>
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60">
+                <AutoRechargeModal setIsModalOpen={setIsModalOpen}/>
+            </div>
         }
     </div>
   );
--- frontend/nextdoor/components/pays/common/Header/index.tsx
+++ frontend/nextdoor/components/pays/common/Header/index.tsx
@@ -2,12 +2,13 @@ import { X } from "lucide-react";
 
 interface HeaderProps {
     txt: string;
+    x?: boolean;
 }
 
-export default function Header({txt}: HeaderProps) {
+export default function Header({txt, x = true}: HeaderProps) {
   return (
     <div className="flex h-[60px] items-center px-4 text-gray900">
-        <X className="w-6 h-6" />
+        {x && <X className="w-6 h-6" />}
         <div className="flex-1 text-center gap-2 ">
             <div className="text-xl font-semibold">{txt}</div>
         </div>
