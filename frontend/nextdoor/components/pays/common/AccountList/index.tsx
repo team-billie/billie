84f344b95be22e27a236c259afd3ae87d1f764f5
안다정 <dksekwjd312@naver.com>
2025-05-08 06:47:57 +0900

    :sparkles: [feat] Pay : AccountList 계좌 추가 버튼 추가

--- frontend/nextdoor/components/pays/common/AccountList/index.tsx
+++ frontend/nextdoor/components/pays/common/AccountList/index.tsx
@@ -1,8 +1,13 @@
 import { bankList } from "@/lib/utils/getBankInfo";
+import { Plus } from "lucide-react"
 
-export default function AccountList() {
+interface AccountListProps {
+    addBtn?: boolean;
+}
+
+export default function AccountList({ addBtn = false }: AccountListProps) {
     const isMainAccount = true;
-    
+
     return (
         <div className="flex flex-col gap-3">
             {bankList.map((bank) => (
@@ -12,13 +17,18 @@ export default function AccountList() {
                             src={bank.image}
                             alt={bank.bankName}
                             className="w-7 h-7 rounded-full"
-                            />
+                        />
                         <div className="text-lg font-bold">{bank.bankName}</div>
                         <div>3333139177983</div>
                     </div>
                     {isMainAccount && <div className="text-white text-xs bg-blue400 px-2 py-1 rounded-lg">주계좌</div>}
                 </div>
             ))}
+            {addBtn &&
+                <div className="flex flex items-center gap-2 p-4 border border-gray400 rounded-lg">
+                    <Plus className="w-6 h-6 p-1 bg-gray400 text-gray700 rounded-full" />
+                    <div className="text-lg font-bold">계좌 추가하기</div>
+                </div>}
         </div>
     );
 }
