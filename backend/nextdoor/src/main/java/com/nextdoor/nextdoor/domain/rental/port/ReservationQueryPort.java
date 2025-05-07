f9b128b9e3827e4416a79f750ac03a274dd82977
Choijake <shameless8@naver.com>
2025-05-08 01:13:06 +0900

    :sparkles: [feat] 예약 데이터 조회하는 ReservationQueryAdapter 구현 #S12P31E205-99

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/port/ReservationQueryPort.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/port/ReservationQueryPort.java
@@ -1,14 +1,10 @@
 package com.nextdoor.nextdoor.domain.rental.port;
 
 import com.nextdoor.nextdoor.domain.rental.service.dto.ReservationDto;
-import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface ReservationQueryPort {
 
-    ReservationDto getReservationByRentalId(Long rentalId);
-    List<ReservationDto> getReservationsByOwnerId(Long ownerId, Pageable pageable);
-    List<ReservationDto> getReservationsByRenterId(Long renterId, Pageable pageable);
-    long countReservations(Long userId, String userRole);
+    Optional<ReservationDto> getReservationByRentalId(Long rentalId);
 }
