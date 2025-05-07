b5bf3cdaedb1c8104f691d0e9127368284a3458d
Choijake <shameless8@naver.com>
2025-05-07 17:58:24 +0900

    :fire: [remove] RentalRepository 사용하지 않는 메서드 삭제 #S12P31E205-99

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/repository/RentalRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/repository/RentalRepository.java
@@ -4,13 +4,11 @@ import com.nextdoor.nextdoor.domain.rental.domain.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long>, RentalCustomRepository {
 
     Optional<Rental> findByRentalId(Long rentalId);
-    Optional<Rental> findByReservationId(Long reservationId);
-    List<Rental> findByReservationIdIn(List<Long> reservationIds);
+
 }
