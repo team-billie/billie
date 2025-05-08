0871b62948048bef009b5290ec4460a5dbe9a80e
5almiakki <jpui9635@gmail.com>
2025-05-08 16:32:33 +0900

    :hammer_and_wrench: [config] Security 관련 설정 #S12P31E205-106

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.nextdoor.nextdoor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(httpRequest -> httpRequest
+                        .requestMatchers(
+                                "/api/v1/auth",
+                                "/api/v1/oauth"
+                        ).permitAll()
+                        // TODO OAuth2 구현 후 아래 줄 수정
+                        .anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .build();
+    }
+}
