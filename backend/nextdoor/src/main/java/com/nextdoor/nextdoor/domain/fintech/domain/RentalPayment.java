0b781b7a6f7c912bb6a3fa0eaf329f0e67b744f4
허준수 <gjwnstn515@naver.com>
2025-05-08 14:54:39 +0900

    :sparkles: [feat] 대여 내역 엔티티 추가 #S12P31E205-115

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/domain/RentalPayment.java
@@ -0,0 +1,44 @@
+package com.nextdoor.nextdoor.domain.fintech.domain;
+
+import com.nextdoor.nextdoor.domain.rental.Rental;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rental_payment")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class RentalPayment {
+    @Id
+    @Column(name = "payment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "rental_id", nullable = false)
+//    private Rental rental;
+    // userId만 칼럼으로 저장 (ManyToOne 제거)
+    @Column(name = "rental_id", nullable = false)
+    private Long rentalId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "payer_regist_account_id", nullable = false)
+    private RegistAccount payer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "payee_regist_account_id", nullable = false)
+    private RegistAccount payee;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private RentalPaymentStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+}
