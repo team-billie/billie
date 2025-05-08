fceecb035b2a53eac98135a3c48b571acd23b10e
허준수 <gjwnstn515@naver.com>
2025-05-09 00:34:11 +0900

    :bug: [fix] LazyInitializationException 에러 해결 #S12P31E205-115

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/RegistAccountRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/RegistAccountRepository.java
@@ -1,6 +1,7 @@
 package com.nextdoor.nextdoor.domain.fintech.repository;
 
 import com.nextdoor.nextdoor.domain.fintech.domain.RegistAccount;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,8 +9,11 @@ import java.util.Optional;
 
 public interface RegistAccountRepository extends JpaRepository<RegistAccount, Long> {
     // 등록된 계좌 전체 조회
+    @EntityGraph(attributePaths = {"account"})
     List<RegistAccount> findByUser_UserKey(String userKey);
+
     // user.userKey 와 account.accountNo 로 조회 ,  등록된 계좌 1개를 찾는거임
+    @EntityGraph(attributePaths = {"account"})
     Optional<RegistAccount> findByUser_UserKeyAndAccount_AccountNo(String userKey, String accountNo);
 
 }
