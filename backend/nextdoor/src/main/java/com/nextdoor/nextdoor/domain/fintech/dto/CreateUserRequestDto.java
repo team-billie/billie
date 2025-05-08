605127477ca08e0b73b8514d64216d26a316de4f
허준수 <gjwnstn515@naver.com>
2025-05-08 22:50:28 +0900

    :sparkles: [feat] 등록 계좌 목록 조회 Dto 추가 #S12P31E205-115

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/CreateUserRequestDto.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/CreateUserRequestDto.java
@@ -3,6 +3,7 @@ package com.nextdoor.nextdoor.domain.fintech.dto;
 import lombok.Data;
 
 @Data
+//계정 생성 Dto
 public class CreateUserRequestDto {
     private Long userId;
     private String ssafyApiEmail; //이걸로 ssafy api 호출할때 body에 해당하는 userId에 넣을거임
