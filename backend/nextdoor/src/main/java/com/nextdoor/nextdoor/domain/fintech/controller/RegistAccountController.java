63c5fe3c0a1ed6e8e6e0835e214a4358d313d03f
허준수 <gjwnstn515@naver.com>
2025-05-08 23:50:37 +0900

    :sparkles: [feat] 계좌 등록 컨트롤러 추가 #S12P31E205-115

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/controller/RegistAccountController.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/controller/RegistAccountController.java
@@ -1,5 +1,6 @@
 package com.nextdoor.nextdoor.domain.fintech.controller;
 
+import com.nextdoor.nextdoor.domain.fintech.dto.RegistAccountRequestDto;
 import com.nextdoor.nextdoor.domain.fintech.dto.RegistAccountResponseDto;
 import com.nextdoor.nextdoor.domain.fintech.service.RegistAccountService;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/fintechs")
 @RequiredArgsConstructor
 public class RegistAccountController {
-    private final RegistAccountService service;
+    private final RegistAccountService registAccountService;
 
     /**
      * 등록된 계좌 목록 조회
@@ -23,7 +24,22 @@ public class RegistAccountController {
     public Mono<ResponseEntity<List<RegistAccountResponseDto>>> listRegistAccounts(
             @RequestParam("userKey") String userKey
     ) {
-        return service.getRegistAccounts(userKey)
+        return registAccountService.getRegistAccounts(userKey)
                 .map(ResponseEntity::ok);
+    }
+
+    /**
+     * 계좌 등록 API
+     * POST /api/v1/fintechs/regist-accounts
+     */
+    @PostMapping("/regist-accounts")
+    public Mono<ResponseEntity<RegistAccountResponseDto>> register(
+            @RequestBody RegistAccountRequestDto req
+    ) {
+        return registAccountService.registerAccount(req)
+                .map(ResponseEntity::ok)
+                .onErrorResume(e -> Mono.just(
+                        ResponseEntity.badRequest().build()
+                ));
     }
 }
