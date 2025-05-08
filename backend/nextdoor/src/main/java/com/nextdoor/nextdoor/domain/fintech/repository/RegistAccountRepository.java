38663846a33e21778c2c9861174189dbb719656c
허준수 <gjwnstn515@naver.com>
2025-05-08 22:47:04 +0900

    :sparkles: [feat] 등록 계좌 레포지토리 추가 #S12P31E205-115

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/RegistAccountRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/RegistAccountRepository.java
@@ -1,11 +1,11 @@
 package com.nextdoor.nextdoor.domain.fintech.repository;
+
 import com.nextdoor.nextdoor.domain.fintech.domain.RegistAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-import java.util.Optional;
 
 public interface RegistAccountRepository extends JpaRepository<RegistAccount, Long> {
+    // userId로 등록된 계좌 전체 조회
     List<RegistAccount> findByUserId(Long userId);
-    // 플랫폼 UserPK(Long userId) + SSAFY 계좌번호(accountNo:String) 조회
-    Optional<RegistAccount> findByUserIdAndAccount_AccountNo(Long userId, String accountNo);
 }
