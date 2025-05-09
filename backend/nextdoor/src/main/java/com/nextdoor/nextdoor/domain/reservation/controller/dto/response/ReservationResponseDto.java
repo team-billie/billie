99ba3cafc6dc0142bc2735c70501b399c8d589e6
5almiakki <jpui9635@gmail.com>
2025-05-09 12:08:00 +0900

    :recycle: [refactor] 예약 조회 응답 DTO 팩토리 메서드 수정

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/controller/dto/response/ReservationResponseDto.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/controller/dto/response/ReservationResponseDto.java
@@ -2,7 +2,6 @@ package com.nextdoor.nextdoor.domain.reservation.controller.dto.response;
 
 import com.nextdoor.nextdoor.domain.reservation.domain.Reservation;
 import com.nextdoor.nextdoor.domain.reservation.service.dto.PostDto;
-import com.nextdoor.nextdoor.domain.reservation.service.dto.MemberDto;
 import com.nextdoor.nextdoor.domain.reservation.service.dto.ReservationQueryDto;
 import lombok.Getter;
 
@@ -23,18 +22,18 @@ public class ReservationResponseDto {
     private String ownerName;
     private String ownerProfileImageUrl;
 
-    public static ReservationResponseDto from(Reservation reservation, PostDto feed, MemberDto owner) {
+    public static ReservationResponseDto from(Reservation reservation, PostDto post) {
         ReservationResponseDto response = new ReservationResponseDto();
         response.reservationId = reservation.getId();
-        response.feedTitle = feed.getTitle();
-        response.feedProductImage = feed.getProductImage();
+        response.feedTitle = post.getTitle();
+        response.feedProductImage = post.getProductImage();
         response.startDate = reservation.getStartDate();
         response.endDate = reservation.getEndDate();
         response.rentalFee = reservation.getRentalFee();
         response.deposit = reservation.getDeposit();
         response.status = reservation.getStatus().name();
-        response.ownerName = owner.getName();
-        response.ownerProfileImageUrl = owner.getProfileImageUrl();
+        response.ownerName = post.getAuthorName();
+        response.ownerProfileImageUrl = post.getAuthorProfileImageUrl();
         return response;
     }
 
