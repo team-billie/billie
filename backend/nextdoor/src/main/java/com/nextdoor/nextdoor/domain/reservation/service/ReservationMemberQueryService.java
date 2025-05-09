7942f158c0d7e26a0dcd79c7488bf817646151c3
5almiakki <jpui9635@gmail.com>
2025-05-09 12:05:06 +0900

    :fire: [remove] 불필요한 쿼리 service 제거

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationMemberQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.nextdoor.nextdoor.domain.reservation.service;
-
-import com.nextdoor.nextdoor.domain.reservation.service.dto.MemberDto;
-
-public interface ReservationMemberQueryService {
-
-    MemberDto findById(Long id);
-}
--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationPostQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.nextdoor.nextdoor.domain.reservation.service;
-
-import com.nextdoor.nextdoor.domain.reservation.service.dto.PostDto;
-
-public interface ReservationPostQueryService {
-
-    PostDto findById(Long feedId);
-}
