7785f12231f15591949ad2c942fe072045eb7fad
5almiakki <jpui9635@gmail.com>
2025-05-09 11:21:05 +0900

    :recycle: [refactor] DB 조회용 클래스명 변경

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/query/ReservationQueryAdapter.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/query/ReservationQueryAdapter.java
@@ -1,41 +1,54 @@
 package com.nextdoor.nextdoor.query;
 
-import com.nextdoor.nextdoor.common.Adapter;
-import com.nextdoor.nextdoor.domain.rental.port.ReservationQueryPort;
-import com.nextdoor.nextdoor.domain.rental.service.dto.ReservationDto;
+import com.nextdoor.nextdoor.domain.reservation.controller.dto.request.ReservationRetrieveRequestDto;
 import com.nextdoor.nextdoor.domain.reservation.domain.QReservation;
-import com.querydsl.core.types.Projections;
+import com.nextdoor.nextdoor.domain.reservation.port.ReservationQueryPort;
+import com.nextdoor.nextdoor.domain.reservation.service.dto.ReservationQueryDto;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
-@Adapter
 @RequiredArgsConstructor
+@Repository
 public class ReservationQueryAdapter implements ReservationQueryPort {
 
-    private final JPAQueryFactory queryFactory;
-    private final QReservation reservation = QReservation.reservation;
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QReservation qReservation = QReservation.reservation;
 
     @Override
-    public Optional<ReservationDto> getReservationByRentalId(Long rentalId) {
-        return Optional.ofNullable(
-                queryFactory
-                        .select(Projections.constructor(
-                                ReservationDto.class,
-                                reservation.id.as("reservationId"),
-                                reservation.startDate,
-                                reservation.endDate,
-                                reservation.rentalFee,
-                                reservation.deposit,
-                                reservation.status.stringValue(),
-                                reservation.ownerId,
-                                reservation.renterId,
-                                reservation.feedId
-                        ))
-                        .from(reservation)
-                        .where(reservation.rentalId.eq(rentalId))
-                        .fetchOne()
-        );
+    public List<ReservationQueryDto> findSentReservations(Long loginUserId, ReservationRetrieveRequestDto requestDto) {
+        return jpaQueryFactory.select(qReservation)
+                .from(qReservation)
+                .where(createSentReservationCondition(loginUserId, requestDto))
+                .fetch()
+                .stream().map(ReservationQueryDto::from).toList();
+    }
+
+    private BooleanBuilder createSentReservationCondition(Long loginUserId, ReservationRetrieveRequestDto requestDto) {
+        BooleanBuilder builder = new BooleanBuilder().and(qReservation.renterId.eq(loginUserId));
+        if (requestDto.getStatus() != null) {
+            builder.and(qReservation.status.eq(requestDto.getStatus()));
+        }
+        return builder;
+    }
+
+    @Override
+    public List<ReservationQueryDto> findReceivedReservations(Long loginUserId, ReservationRetrieveRequestDto requestDto) {
+        return jpaQueryFactory.select(qReservation)
+                .from(qReservation)
+                .where(createReceivedReservationCondition(loginUserId, requestDto))
+                .fetch()
+                .stream().map(ReservationQueryDto::from).toList();
+    }
+
+    private BooleanBuilder createReceivedReservationCondition(Long loginUserId, ReservationRetrieveRequestDto requestDto) {
+        BooleanBuilder builder = new BooleanBuilder().and(qReservation.ownerId.eq(loginUserId));
+        if (requestDto.getStatus() != null) {
+            builder.and(qReservation.status.eq(requestDto.getStatus()));
+        }
+        return builder;
     }
 }
