9e31375d0e90b92e3086833ca8028f0f5dc2d405
Choijake <shameless8@naver.com>
2025-05-09 15:34:19 +0900

    :sparkles: [feat] PostLike 엔티티 추가 #S12P31E205-100

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/domain/TimestampedEntity.java
@@ -0,0 +1,22 @@
+package com.nextdoor.nextdoor.domain.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class TimestampedEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @NotNull
+    private LocalDateTime createdAt;
+}
