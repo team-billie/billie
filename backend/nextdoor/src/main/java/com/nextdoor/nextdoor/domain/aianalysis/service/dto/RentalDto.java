c0c74e5fa101133dfb0f0bc22089e108b66a719d
5almiakki <jpui9635@gmail.com>
2025-05-09 15:32:38 +0900

    :sparkles: [feat] AI 분석 도메인의 대여 DTO에 생성자 추가

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/dto/RentalDto.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/dto/RentalDto.java
@@ -1,10 +1,12 @@
 package com.nextdoor.nextdoor.domain.aianalysis.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Builder
 @Getter
 public class RentalDto {
@@ -15,6 +17,7 @@ public class RentalDto {
     private String damageAnalysis;
     private List<AiImageDto> aiImages;
 
+    @AllArgsConstructor
     @Builder
     @Getter
     public static class AiImageDto {
