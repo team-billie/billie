5b71f08b3cc2c55c360b5810075a682ad136275b
허준수 <gjwnstn515@naver.com>
2025-05-11 02:08:39 +0900

    :sparkles: [feat] 채팅방 복합키 정의 엔티티 추가 #S12P31E205-101

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/domain/ChatMessageKey.java
@@ -0,0 +1,23 @@
+package com.nextdoor.nextdoor.chat.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.mapping.Table;
+import java.time.Instant;
+
+import static org.springframework.data.cassandra.core.cql.PrimaryKeyType.PARTITIONED;
+//import static org.springframework.data.cassandra.core.mapping.PrimaryKeyType.PARTITIONED;
+
+@PrimaryKeyClass
+@Data @NoArgsConstructor @AllArgsConstructor
+public class ChatMessageKey {
+    @PrimaryKeyColumn(name = "conversation_id", ordinal = 0, type = PARTITIONED)
+    private String conversationId;
+
+    @PrimaryKeyColumn(name = "sent_at", ordinal = 1)
+    private Instant sentAt;
+}
