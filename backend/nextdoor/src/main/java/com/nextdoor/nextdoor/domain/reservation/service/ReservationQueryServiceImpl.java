18d3303b185e49d38ca856d4351dada258129ab3
5almiakki <jpui9635@gmail.com>
2025-05-10 17:07:54 +0900

    :sparkles: [feat] 대여 service에 일정 조회 로직 추가

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationQueryServiceImpl.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/service/ReservationQueryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nextdoor.nextdoor.domain.reservation.service;
 
+import com.nextdoor.nextdoor.domain.reservation.controller.dto.request.ReservationCalendarRetrieveRequestDto;
 import com.nextdoor.nextdoor.domain.reservation.controller.dto.request.ReservationRetrieveRequestDto;
+import com.nextdoor.nextdoor.domain.reservation.controller.dto.response.ReservationCalendarResponseDto;
 import com.nextdoor.nextdoor.domain.reservation.controller.dto.response.ReservationResponseDto;
 import com.nextdoor.nextdoor.domain.reservation.port.ReservationQueryPort;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,13 @@ public class ReservationQueryServiceImpl implements ReservationQueryService {
     public List<ReservationResponseDto> retrieveReceivedReservations(Long loginUserId, ReservationRetrieveRequestDto requestDto) {
         return reservationQueryPort.findReceivedReservations(loginUserId, requestDto).stream()
                 .map(ReservationResponseDto::from).toList();
+    }
+
+    @Override
+    public ReservationCalendarResponseDto retrieveReservationCalendar(Long loginUserId, ReservationCalendarRetrieveRequestDto requestDto) {
+        List<ReservationCalendarResponseDto.Period> periods = reservationQueryPort.findReservationCalendar(requestDto).stream()
+                .map(reservation -> new ReservationCalendarResponseDto.Period(reservation.getStartDate(), reservation.getEndDate()))
+                .toList();
+        return new ReservationCalendarResponseDto(periods);
     }
 }
