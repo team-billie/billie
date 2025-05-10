d3ec7d3961e6e55570ddffe6d35e3ec8592adfa8
허준수 <gjwnstn515@naver.com>
2025-05-11 03:05:25 +0900

    :sparkles: [feat] 패키지 구조 변경 및 채팅메세지 레포지토리 추가 #S12P31E205-101

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/persistence/ChatMessageRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/persistence/ChatMessageRepository.java
@@ -5,7 +5,12 @@ import com.nextdoor.nextdoor.domain.chat.domain.ChatMessageKey;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatMessageRepository extends CassandraRepository<ChatMessage, ChatMessageKey> {
-    // 필요 시, 커스텀 쿼리 메소드 추가 가능
+    // 특정 대화방 메시지 페이징 조회
+    List<ChatMessage> findByKeyConversationId(String conversationId);
+
+    // (필요시) 안 읽은 메시지 개수 조회 등 추가 메서드 선언
 }
