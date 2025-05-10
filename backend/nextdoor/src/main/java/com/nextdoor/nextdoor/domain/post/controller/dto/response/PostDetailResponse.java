b11256ad8d554919304487a93d4dc5b5b0a7e4f5
Choijake <shameless8@naver.com>
2025-05-11 02:19:18 +0900

    :sparkles: [feat] 포스트 조회 응답 수정 #S12P31E205-100 - PostDetailResponse.java

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/controller/dto/response/PostDetailResponse.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/controller/dto/response/PostDetailResponse.java
@@ -3,6 +3,8 @@ package com.nextdoor.nextdoor.domain.post.controller.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class PostDetailResponse {
@@ -12,7 +14,7 @@ public class PostDetailResponse {
     private Long deposit;
     private String address;
     private String location;
-    private String productImage;
+    private List<String> productImage;
     private String category;
     private Long authorId;
     private String nickname;
