1d75e2de5e0801fc3e09f305ea18eeef5429c726
5almiakki <jpui9635@gmail.com>
2025-05-11 03:45:13 +0900

    :sparkles: [feat] OAuth2 인증 성공 시 핸들러 추가 #S12P31E205-106

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/auth/OAuth2SuccessHandler.java
@@ -0,0 +1,43 @@
+package com.nextdoor.nextdoor.domain.auth;
+
+import com.nextdoor.nextdoor.domain.auth.exception.RedirectUrlNotPresentException;
+import com.nextdoor.nextdoor.domain.auth.service.JwtProvider;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static com.nextdoor.nextdoor.domain.auth.filter.RedirectUrlCookieFilter.REDIRECT_URI_PARAM;
+
+@AllArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String accessToken = jwtProvider.createAccessToken(authentication);
+        Optional<Cookie> oCookie = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(REDIRECT_URI_PARAM)).findFirst();
+        Optional<String> redirectUrl = oCookie.map(Cookie::getValue);
+        response.sendRedirect(redirectUrl.orElseThrow(RedirectUrlNotPresentException::new)
+                + "/api/v1/auth/sociallogin?token=" + accessToken);
+        clearAuthenticationAttributes(request, response);
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        oAuth2AuthorizationRequestBasedOnCookieRepository.removeAuthorizationRequestCookies(request, response);
+    }
+}
