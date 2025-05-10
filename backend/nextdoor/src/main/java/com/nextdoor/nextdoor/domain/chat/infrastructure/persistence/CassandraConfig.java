c4ba870abbac1420ba3ac2cf1fc384a10d4da24a
허준수 <gjwnstn515@naver.com>
2025-05-11 02:46:11 +0900

    :sparkles: [feat] CassandraConfig 설정 변경 #S12P31E205-101

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/persistence/CassandraConfig.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/persistence/CassandraConfig.java
@@ -1,5 +1,6 @@
 package com.nextdoor.nextdoor.domain.chat.infrastructure.persistence;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
@@ -7,13 +8,41 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @Configuration
 @EnableCassandraRepositories(basePackages = "com.nextdoor.nextdoor.domain.chat.infrastructure.persistence")
 public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.cassandra.keyspace-name}")
+    private String keyspace;
+
+    @Value("${spring.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.cassandra.port}")
+    private int port;
+
+    @Value("${spring.cassandra.local-datacenter}")
+    private String datacenter;
+
     @Override
     protected String getKeyspaceName() {
-        return "nextdoor_chat";
+        return keyspace;
     }
 
     @Override
-    public String[] getEntityBasePackages() {
-        return new String[]{"com.nextdoor.nextdoor.domain.chat.domain"};
+    protected String getContactPoints() {
+        return contactPoints;
     }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return datacenter;
+    }
+
+//    @Override
+//    public String[] getEntityBasePackages() {
+//        return new String[]{"com.nextdoor.nextdoor.domain.chat.domain"};
+//    }
 }
