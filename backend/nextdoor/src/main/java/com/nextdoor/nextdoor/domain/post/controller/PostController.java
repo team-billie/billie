11a9f9346b53954db152d31518cda087938ec3cb
Choijake <shameless8@naver.com>
2025-05-11 01:40:19 +0900

    :sparkles: [feat] 포스트 등록 api 추가 #S12P31E205-100 - PostController.java

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/controller/PostController.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/controller/PostController.java
@@ -1,19 +1,28 @@
 package com.nextdoor.nextdoor.domain.post.controller;
 
+import com.nextdoor.nextdoor.domain.post.controller.dto.request.CreatePostRequest;
+import com.nextdoor.nextdoor.domain.post.controller.dto.response.CreatePostResponse;
 import com.nextdoor.nextdoor.domain.post.controller.dto.response.PostDetailResponse;
 import com.nextdoor.nextdoor.domain.post.controller.dto.response.PostListResponse;
 import com.nextdoor.nextdoor.domain.post.mapper.PostMapper;
 import com.nextdoor.nextdoor.domain.post.service.PostService;
+import com.nextdoor.nextdoor.domain.post.service.dto.CreatePostCommand;
+import com.nextdoor.nextdoor.domain.post.service.dto.CreatePostResult;
 import com.nextdoor.nextdoor.domain.post.service.dto.PostDetailResult;
 import com.nextdoor.nextdoor.domain.post.service.dto.SearchPostCommand;
 import com.nextdoor.nextdoor.domain.post.service.dto.SearchPostResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -40,6 +49,19 @@ public class PostController {
     public ResponseEntity<PostDetailResponse> getPostDetail(@PathVariable Long postId) {
         PostDetailResult result = postService.getPostDetail(postId);
         PostDetailResponse response = postMapper.toDetailResponse(result);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CreatePostResponse> createPost(
+            @RequestPart("post") @Valid CreatePostRequest request,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images,
+            @RequestParam Long authorId
+    ) {
+        CreatePostCommand command = postMapper.toCreateCommand(request, images, authorId);
+        CreatePostResult result = postService.createPost(command);
+        CreatePostResponse response = postMapper.toCreateResponse(result);
 
         return ResponseEntity.ok(response);
     }
