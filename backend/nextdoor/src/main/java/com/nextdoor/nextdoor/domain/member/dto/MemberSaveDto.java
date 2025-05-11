2ae62e04debfab84549c0b0d43f23a9c9bb43f2d
5almiakki <jpui9635@gmail.com>
2025-05-12 02:38:59 +0900

    :sparkles: [feat] 사용자 추가 DTO 추가 #S12P31E205-106

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/member/dto/MemberSaveDto.java
@@ -0,0 +1,11 @@
+package com.nextdoor.nextdoor.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MemberSaveDto {
+
+    private String nickname;
+}
