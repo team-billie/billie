32a53290379491f7432501a575e1471f9e7c7475
안다정 <dksekwjd312@naver.com>
2025-05-11 21:12:57 +0900

    :sparkles: [feat] 게시글 목록 요청 api 작성(1/2) #S12P31E205-82

--- frontend/nextdoor/app/(tabs)/home/page.tsx
+++ frontend/nextdoor/app/(tabs)/home/page.tsx
@@ -1,4 +1,9 @@
+"use-client"
+
 import { Metadata } from "next";
+import { GetPostListRequest } from "@/lib/api/posts";
+import { useEffect, useState } from "react";
+
 
 export function generateMetadata(): Metadata {
   return {
@@ -8,6 +13,12 @@ export function generateMetadata(): Metadata {
 }
 
 export default function HomePage() {
+  const [postList, setPostList] = useState<PostListResponseDto[]>([]);
+  useEffect(() => {
+    GetPostListRequest("1").then((res) => {
+      setPostList(res);
+    });
+  }, []);
   return (
     <main className="p-24">
       <h1 className="text-4xl font-bold">Home</h1>
---
+++ frontend/nextdoor/types/posts/response/index.ts
@@ -0,0 +1,7 @@
+type PostListResponseDto = {
+    id: number;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+}
