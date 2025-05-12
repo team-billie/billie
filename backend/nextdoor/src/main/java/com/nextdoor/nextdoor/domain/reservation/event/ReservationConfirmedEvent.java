ae01b44d2c632a66969dbf5dd0ce252cb41d59a8
Choijake <shameless8@naver.com>
2025-05-12 12:29:32 +0900

    :sparkles: [feat] 예약 확정 이벤트에 빌더 추가 #S12P31E205-99 - ReservationConfirmedEvent.java

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/event/ReservationConfirmedEvent.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/event/ReservationConfirmedEvent.java
@@ -1,11 +1,12 @@
 package com.nextdoor.nextdoor.domain.reservation.event;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
-import java.util.Date;
 
+@Builder
 @AllArgsConstructor
 @Getter
 public class ReservationConfirmedEvent {
