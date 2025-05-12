bd91b70a421ab79cff8b3845b25d82555bb9eb55
5almiakki <jpui9635@gmail.com>
2025-05-12 17:28:33 +0900

    :sparkles: [feat] 인증 인가 도메인의 사용자 변경 포트 어댑터 추가 #S12P31E205-106

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/command/AuthMemberCommandAdapter.java
@@ -0,0 +1,27 @@
+package com.nextdoor.nextdoor.command;
+
+import com.nextdoor.nextdoor.common.Adapter;
+import com.nextdoor.nextdoor.domain.auth.port.AuthMemberCommandPort;
+import com.nextdoor.nextdoor.domain.auth.service.dto.MemberCommandDto;
+import com.nextdoor.nextdoor.domain.auth.service.dto.MemberQueryDto;
+import com.nextdoor.nextdoor.domain.member.domain.Member;
+import com.nextdoor.nextdoor.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Adapter
+public class AuthMemberCommandAdapter implements AuthMemberCommandPort {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberQueryDto save(MemberCommandDto command) {
+        Member member = memberRepository.save(Member.builder()
+                .authProvider(command.getAuthProvider())
+                .nickname(command.getNickname())
+                .email(command.getEmail())
+                .profileImageUrl(command.getProfileImageUrl())
+                .build());
+        return new MemberQueryDto(member.getId(), member.getEmail());
+    }
+}
