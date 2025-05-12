6c3259dddf1fc6481e7f8d20617c90295c6d6328
Choijake <shameless8@naver.com>
2025-05-12 12:27:45 +0900

    :hammer_and_wrench: [config] 트랜잭션 템플릿 추가 #S12P31E205-99 - TransactionConfig.java

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/TransactionConfig.java
@@ -0,0 +1,16 @@
+package com.nextdoor.nextdoor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Configuration
+public class TransactionConfig {
+
+    @Bean
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+        TransactionTemplate template = new TransactionTemplate(transactionManager);
+        return template;
+    }
+}
