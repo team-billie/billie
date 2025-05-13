248d5be8fa42f212097c03835d7507910f5e409c
5almiakki <jpui9635@gmail.com>
2025-05-14 00:39:10 +0900

    :sparkles: [feat] 손상 분석 로직 추가 #S12P31E205-125
    손상 비교 로직과 구분

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/AiAnalysisService.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/AiAnalysisService.java
@@ -1,9 +1,12 @@
 package com.nextdoor.nextdoor.domain.aianalysis.service;
 
-import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.request.InspectDamageRequestDto;
+import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.request.DamageAnalysisRequestDto;
+import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.request.DamageComparisonRequestDto;
 import com.nextdoor.nextdoor.domain.aianalysis.controller.dto.response.InspectDamageResponseDto;
 
 public interface AiAnalysisService {
 
-    InspectDamageResponseDto inspectDamage(Long loginUserId, InspectDamageRequestDto inspectDamageRequestDto);
+    InspectDamageResponseDto analyzeDamage(Long loginUserId, DamageAnalysisRequestDto damageAnalysisRequestDto);
+
+    InspectDamageResponseDto compareDamage(Long loginUserId, DamageComparisonRequestDto damageComparisonRequestDto);
 }
