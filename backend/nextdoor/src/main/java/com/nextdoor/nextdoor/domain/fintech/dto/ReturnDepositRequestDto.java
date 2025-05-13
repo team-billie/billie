7efbf3934958513bccfa68cabf85d068e1438ecf
Choijake <shameless8@naver.com>
2025-05-13 11:13:48 +0900

    :sparkles: [feat] 보증금 반환 시 rentalId로 Deposit 조회하도록 수정 #S12P31E205-99

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/ReturnDepositRequestDto.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/dto/ReturnDepositRequestDto.java
@@ -5,10 +5,9 @@ import lombok.Data;
 // 보증금 반환 Dto
 @Data
 public class ReturnDepositRequestDto {
-    private Long renterId;           // 렌터의 내부 PK
-    private Long depositId;          // 보증금 내역 PK
-    private Long deductedAmount;     // 차감액 (없으면 0 또는 null)
-    private String userKey;     // 오너의 SSAFY userKey
-    private String accountNo;   // 오너가 받을 계좌번호
-    private Long rentalId;           // 대여 내역 ID
+    private String userKey;            // 렌터의 SSAFY userKey
+    private Long deductedAmount;       // 차감액 (없으면 0 또는 null)
+    private String ownerUserKey;       // 오너의 SSAFY userKey 추가
+    private String ownerAccountNo; //차감액을 받을 오너 계좌번호
+    private Long rentalId;                    // 결제와 관련된 rentalId
 }
--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/DepositRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/DepositRepository.java
@@ -1,17 +1,19 @@
 package com.nextdoor.nextdoor.domain.fintech.repository;
+
 import com.nextdoor.nextdoor.domain.fintech.domain.Deposit;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
+
     @Query("SELECT d FROM Deposit d "
             + " JOIN FETCH d.registAccount ra"
             + " JOIN FETCH ra.account a"
-            + " WHERE d.id = :id")
-    Optional<Deposit> findWithAccount(@Param("id") Long id);
+            + " WHERE d.rentalId = :rentalId")
+    Optional<Deposit> findByRentalId(@Param("rentalId") Long rentalId);
+
+    Optional<Long> findIdByRentalId(Long rentalId);
 }
