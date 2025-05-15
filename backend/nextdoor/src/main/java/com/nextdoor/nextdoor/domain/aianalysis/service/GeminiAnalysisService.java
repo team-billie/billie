a0ac06c775600ed7b4336eb176874d0c27a120a1
Choijake <shameless8@naver.com>
2025-05-15 09:15:16 +0900

    :sparkles: [feat] ai 이미지 비교 분석 서비스에 트랜잭션 경게 지정 #S12P31E205-99 - GeminiAnalysisService.java

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/GeminiAnalysisService.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/service/GeminiAnalysisService.java
@@ -70,6 +70,7 @@ public class GeminiAnalysisService implements AiAnalysisService {
         return new InspectDamageResponseDto(damageAnalysis);
     }
 
+    @Transactional(readOnly = true)
     private Content createAnalysisContent(List<RentalDto.AiImageDto> aiImages, AiImageType aiImageType) {
         List<Part> imageParts = aiImages.stream()
                 .filter(aiImageDto -> aiImageDto.getType().equals(aiImageType))
