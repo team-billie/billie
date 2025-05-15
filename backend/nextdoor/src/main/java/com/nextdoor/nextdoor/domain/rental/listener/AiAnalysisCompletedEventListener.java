ff71038f34257c98f197885795eb98aff883a7cc
Choijake <shameless8@naver.com>
2025-05-15 23:38:22 +0900

    :bug: [fix] ai 분석 후 이벤트 핸들링 실패 이슈 해결

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/listener/AiAnalysisCompletedEventListener.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/listener/AiAnalysisCompletedEventListener.java
@@ -1,16 +1,12 @@
 package com.nextdoor.nextdoor.domain.rental.listener;
 
 import com.nextdoor.nextdoor.domain.aianalysis.event.out.AiAnalysisCompletedEvent;
-import com.nextdoor.nextdoor.domain.rental.domain.RentalProcess;
-import com.nextdoor.nextdoor.domain.rental.domain.RentalStatus;
-import com.nextdoor.nextdoor.domain.rental.message.RentalStatusMessage;
 import com.nextdoor.nextdoor.domain.rental.service.RentalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -20,19 +16,11 @@ public class AiAnalysisCompletedEventListener {
     private final SimpMessagingTemplate messagingTemplate;
 
     @Async("asyncExecutor")
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+    @EventListener
     public void handleAiAnalysisCompletedEvent(AiAnalysisCompletedEvent aiAnalysisCompletedEvent) {
         rentalService.updateDamageAnalysis(
                 aiAnalysisCompletedEvent.getRentalId(),
                 aiAnalysisCompletedEvent.getDamageAnalysis()
-        );
-
-        messagingTemplate.convertAndSend(
-                "/topic/rental/" + aiAnalysisCompletedEvent.getRentalId() + "/status",
-                RentalStatusMessage.builder()
-                        .process(RentalProcess.BEFORE_RENTAL.name())
-                        .detailStatus(RentalStatus.BEFORE_PHOTO_ANALYZED.name())
-                        .build()
         );
     }
 }
--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/listener/AiCompareAnalysisCompletedEventListener.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/listener/AiCompareAnalysisCompletedEventListener.java
@@ -1,41 +1,24 @@
 package com.nextdoor.nextdoor.domain.rental.listener;
 
-import com.nextdoor.nextdoor.domain.aianalysis.event.out.AiAnalysisCompletedEvent;
 import com.nextdoor.nextdoor.domain.aianalysis.event.out.AiCompareAnalysisCompletedEvent;
-import com.nextdoor.nextdoor.domain.rental.domain.RentalProcess;
-import com.nextdoor.nextdoor.domain.rental.domain.RentalStatus;
-import com.nextdoor.nextdoor.domain.rental.message.RentalStatusMessage;
 import com.nextdoor.nextdoor.domain.rental.service.RentalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class AiCompareAnalysisCompletedEventListener {
 
     private final RentalService rentalService;
-    private final SimpMessagingTemplate messagingTemplate;
 
     @Async("asyncExecutor")
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
-    public void handleAiAnalysisCompletedEvent(AiCompareAnalysisCompletedEvent aiCompareAnalysisCompletedEvent) {
-        rentalService.updateDamageAnalysis(
+    @EventListener
+    public void handleAiCompareAnalysisCompletedEvent(AiCompareAnalysisCompletedEvent aiCompareAnalysisCompletedEvent) {
+        rentalService.updateComparedAnalysis(
                 aiCompareAnalysisCompletedEvent.getRentalId(),
                 aiCompareAnalysisCompletedEvent.getDamageAnalysis()
-        );
-
-        messagingTemplate.convertAndSend(
-                "/topic/rental/" + aiCompareAnalysisCompletedEvent.getRentalId() + "/status",
-                RentalStatusMessage.builder()
-                        .process(RentalProcess.RETURNED.name())
-                        .detailStatus(RentalStatus.BEFORE_AND_AFTER_COMPARED.name())
-                        .build()
         );
     }
 }
