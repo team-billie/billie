5502b40fd3e933ec0484e3f257c79ba5f3f7f9cc
5almiakki <jpui9635@gmail.com>
2025-05-15 11:49:06 +0900

    :sparkles: [feat] 예약 웹소켓 설정 클래스명 변경 #S12P31E205-139

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/config/ReservationWebSocketConfig.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/reservation/config/ReservationWebSocketConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class ReservationWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
