bc19e388595471afc0b89f2773a8589464f5bf7e
diharong <diharong01@gmail.com>
2025-05-15 09:24:53 +0900

    :sparkles: [feat] 위젯 레이아웃 및 드래그, pwa 규격 설정 #S12P31E205-144

--- frontend/nextdoor/app/layout.tsx
+++ frontend/nextdoor/app/layout.tsx
@@ -1,10 +1,12 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
 import { AlertModalProvider } from "@/lib/providers/AlertModalProvider";
+import DraggableWidget from "@/components/common/RentalWidget";
 
 export const viewport = {
   themeColor: "#ffffff",
-  width: "device-width",
+  width: "device-width",  
   initialScale: 1,
   minimumScale: 1,
   shrinkToFit: "no",
@@ -27,8 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-[100dvh] max-w-md mx-auto bg-white">
-        <AlertModalProvider>{children}</AlertModalProvider>
+      <body className="min-h-[100dvh] max-w-md mx-auto bg-white relative">
+        <AlertModalProvider>
+          {children}
+          <DraggableWidget />
+        </AlertModalProvider>
       </body>
     </html>
   );
