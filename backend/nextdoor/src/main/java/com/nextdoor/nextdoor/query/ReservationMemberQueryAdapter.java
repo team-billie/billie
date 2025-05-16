981a2e1c54c81a60e44ffe07f042c7f43cf861b4
5almiakki <jpui9635@gmail.com>
2025-05-16 15:53:04 +0900

    :sparkles: [feat] 예약 도메인 사용자 조회 어댑터 포트 추가

---
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/query/ReservationMemberQueryAdapter.java
@@ -0,0 +1,38 @@
+package com.nextdoor.nextdoor.query;
+
+import com.nextdoor.nextdoor.common.Adapter;
+import com.nextdoor.nextdoor.domain.member.domain.QMember;
+import com.nextdoor.nextdoor.domain.reservation.port.ReservationMemberQueryPort;
+import com.nextdoor.nextdoor.domain.reservation.service.dto.ReservationMemberQueryDto;
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Adapter
+public class ReservationMemberQueryAdapter implements ReservationMemberQueryPort {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QMember qMember = QMember.member;
+
+    @Override
+    public Optional<ReservationMemberQueryDto> findById(Long id) {
+        return Optional.ofNullable(jpaQueryFactory.select(createReservationMemberQueryDtoProjection())
+                .from(qMember)
+                .where(qMember.id.eq(id))
+                .fetchOne());
+    }
+
+    private ConstructorExpression<ReservationMemberQueryDto> createReservationMemberQueryDtoProjection() {
+        return Projections.constructor(
+                ReservationMemberQueryDto.class,
+                qMember.id,
+                qMember.nickname,
+                qMember.profileImageUrl
+        );
+    }
+}
