05e4ac77587cb041d73084c91c00ed706eb44ef0
Choijake <shameless8@naver.com>
2025-05-17 11:36:44 +0900

    :sparkles: [feat] Rental 진행 흐름에서 위젯에게 추가 정보를 브로드캐스트하도록 수정

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/message/RentalStatusMessage.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/message/RentalStatusMessage.java
@@ -12,4 +12,15 @@ public class RentalStatusMessage {
     private Long rentalId;
     private String process;
     private String detailStatus;
+    private RentalDetailResult rentalDetail;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RentalDetailResult {
+        private String postTitle;
+        private String representativeImageUrl;
+        private String partnerProfileImageUrl;
+    }
 }
