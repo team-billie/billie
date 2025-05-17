bbc75b1b529de06b463547cc0dde4571288c7313
5almiakki <jpui9635@gmail.com>
2025-05-17 20:11:37 +0900

    :bug: [fix] Gemini 모델 구분 수정 #S12P31E205-125

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/GeminiConfig.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/GeminiConfig.java
@@ -15,11 +15,11 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class GeminiConfig {
 
-    @Value("${custom.google.ai.gemini.model.analysis}")
-    private String geminiAnalysisModel;
+    @Value("${custom.google.ai.gemini.model.flash}")
+    private String geminiFlash;
 
-    @Value("${custom.google.ai.gemini.model.comparison}")
-    private String geminiComparisonModel;
+    @Value("${custom.google.ai.gemini.model.pro}")
+    private String geminiPro;
 
     @Value("${custom.google.ai.gemini.location}")
     private String geminiLocation;
@@ -41,14 +41,14 @@ public class GeminiConfig {
         return new VertexAI(geminiProjectId, geminiLocation);
     }
 
-    @Bean(name = "geminiAnalysisModel")
+    @Bean(name = "geminiFlash")
     public GenerativeModel geminiAnalysisModel(VertexAI vertexAI) {
-        return new GenerativeModel(geminiAnalysisModel, vertexAI);
+        return new GenerativeModel(geminiFlash, vertexAI);
     }
 
-    @Bean(name = "geminiComparisonModel")
+    @Bean(name = "geminiPro")
     public GenerativeModel geminiComparisonModel(VertexAI vertexAI) {
-        return new GenerativeModel(geminiComparisonModel, vertexAI);
+        return new GenerativeModel(geminiPro, vertexAI);
     }
 
     @Bean(name = "damageAnalyzerPromptPart")
