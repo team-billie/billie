909cd5efa453411c926f5f2c4a2de46584868cda
안다정 <dksekwjd312@naver.com>
2025-05-19 06:32:41 +0900

    :sparkles: [feat] alert 컴포넌트 구현

--- frontend/nextdoor/app/pays/testpage/page.tsx
+++ frontend/nextdoor/app/pays/testpage/page.tsx
@@ -1,18 +1,51 @@
 "use client"
 
 import HandleDepositModal from "@/components/pays/modals/HandleDepositModal";
+import AlertMessage from "@/components/common/AlertMessage";
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
 export default function TestPage() {
-    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isAlertMessageOpen, setIsAlertMessageOpen] = useState(false);
+    const [alertExiting, setAlertExiting] = useState(false);
+
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+        if (isAlertMessageOpen) {
+            // 2초 후에 알림 메시지 닫기 시작
+            timer = setTimeout(() => {
+                // 먼저 exit 애니메이션 시작
+                setAlertExiting(true);
+
+                // exit 애니메이션 완료 후 알림 메시지 완전히 제거
+                setTimeout(() => {
+                    setIsAlertMessageOpen(false);
+                    setAlertExiting(false);
+                }, 500); // 애니메이션 지속시간 
+            }, 2000);
+        }
+
+        return () => clearTimeout(timer);
+    }, [isAlertMessageOpen]);
+
+    const handleShowAlert = () => {
+        if (!isAlertMessageOpen) {
+            setIsAlertMessageOpen(true);
+        }
+    };
+
     return (
         <div className="relative flex flex-col min-h-screen items-center justify-center px-10">
-            <button onClick={() => setIsModalOpen(true)}>모달 열기</button>
-            {isModalOpen && 
-                <div className="absolute inset-0 bg-gray900 flex-1 flex items-center justify-center p-10 bg-opacity-70" >
-                    <HandleDepositModal charge={10000} rentalImg="" rentalId={1} renterId={1} setIsModalOpen={setIsModalOpen}/>
-                </div>
-            }
+            <button
+                onClick={handleShowAlert}
+                className="bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-500"
+            >
+                AlertMessage 띄우기
+            </button>
+
+            {(isAlertMessageOpen) && (
+                <AlertMessage txt="결제가 요청되었습니다!" exiting={alertExiting} />
+            )}
         </div>
-    )
+    );
 }
