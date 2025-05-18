f078a671aed38a1fa491d415f55dcd149ddcdbcb
5almiakki <jpui9635@gmail.com>
2025-05-18 17:33:59 +0900

    :sparkles: [feat] RestTemplate 추가 #S12P31E205-125
    RestTemplate으로 Flask API 호출

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/command/AiAnalysisMatcherCommandAdapter.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/command/AiAnalysisMatcherCommandAdapter.java
@@ -4,12 +4,17 @@ import com.nextdoor.nextdoor.common.Adapter;
 import com.nextdoor.nextdoor.domain.aianalysis.port.AiAnalysisMatcherCommandPort;
 import com.nextdoor.nextdoor.domain.aianalysis.service.dto.ImageMatcherRequestDto;
 import com.nextdoor.nextdoor.domain.aianalysis.service.dto.ImageMatcherResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
 @Adapter
 public class AiAnalysisMatcherCommandAdapter implements AiAnalysisMatcherCommandPort {
 
+    private final RestTemplate restTemplate;
+
     @Override
     public ImageMatcherResponseDto match(ImageMatcherRequestDto request) {
-        return null;
+        return restTemplate.postForObject("http://k12e205.p.ssafy.io/match", request, ImageMatcherResponseDto.class);
     }
 }
