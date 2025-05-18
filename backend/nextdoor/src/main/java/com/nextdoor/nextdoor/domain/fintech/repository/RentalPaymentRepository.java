ac288e361d0da8ead7bb2eb6fab0fb85012453e3
허준수 <gjwnstn515@naver.com>
2025-05-18 20:28:04 +0900

    :fire: [remove] rentalPayment 리포지토리 삭제 #S12P31E205-159

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/fintech/repository/RentalPaymentRepository.java
+++
@@ -1,4 +0,0 @@
-package com.nextdoor.nextdoor.domain.fintech.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalPaymentRepository extends JpaRepository<RentalPayment, Long> {}
