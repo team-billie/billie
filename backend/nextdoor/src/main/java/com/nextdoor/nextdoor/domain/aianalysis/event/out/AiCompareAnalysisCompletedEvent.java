79cfd1f72fe793b435932d578b146f7173b8727c
5almiakki <jpui9635@gmail.com>
2025-05-19 00:48:07 +0900

    :sparkles: [feat] 손상 비교 완료 이벤트 필드 수정 #S12P31E205-125

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/event/out/AiCompareAnalysisCompletedEvent.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/aianalysis/event/out/AiCompareAnalysisCompletedEvent.java
@@ -3,10 +3,22 @@ package com.nextdoor.nextdoor.domain.aianalysis.event.out;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 public class AiCompareAnalysisCompletedEvent {
 
     private Long rentalId;
-    private String damageAnalysis;
+    private String overallComparisonResult;
+    private List<MatchingResult> matchingResults;
+
+    @AllArgsConstructor
+    @Getter
+    public static class MatchingResult {
+
+        private Long aiImageId;
+        private Long comparisonTargetId;
+        private String comparisonResult;
+    }
 }
