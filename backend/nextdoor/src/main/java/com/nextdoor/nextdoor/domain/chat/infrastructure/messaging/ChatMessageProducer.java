29ffe97379d6548d3b49e2c4bea09afbbf07b0cf
허준수 <gjwnstn515@naver.com>
2025-05-20 01:02:36 +0900

    :sparkles: [feat] chatMessageProducer 코드 수정 #S12P31E205-162

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/messaging/ChatMessageProducer.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/chat/infrastructure/messaging/ChatMessageProducer.java
@@ -1,29 +1,27 @@
-//package com.nextdoor.nextdoor.domain.chat.infrastructure.messaging;
-//
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.stereotype.Component;
-//
-//import com.nextdoor.nextdoor.domain.chat.domain.ChatMessage;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//public class ChatMessageProducer {
-//
-//    private final RabbitTemplate rabbitTemplate;
-//
-//    /**
-//     * 메시지를 RabbitMQ로 발행합니다.
-//     */
-//    public void send(ChatMessage chatMessage) {
-//        log.debug("RabbitMQ로 메시지 발행 → {}", chatMessage);
-//        rabbitTemplate.convertAndSend(
-//                RabbitMQConfig.EXCHANGE_NAME,
-//                RabbitMQConfig.ROUTING_KEY,
-//                chatMessage
-//        );
-//    }
-//}
+package com.nextdoor.nextdoor.domain.chat.infrastructure.messaging;
+
+import com.nextdoor.nextdoor.domain.chat.config.RabbitMQConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * RabbitMQ로 ChatMessagePayload 발행
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatMessageProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(ChatMessagePayload payload) {
+        log.debug("RabbitMQ로 메시지 발행 → {}", payload);
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.CHAT_EXCHANGE,
+                RabbitMQConfig.CHAT_ROUTING_KEY,
+                payload
+        );
+    }
+}
