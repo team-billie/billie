390d2a8ffd266d7f336a618a3590e2cddf398c0a
Choijake <shameless8@naver.com>
2025-05-20 01:17:54 +0900

    :sparkles: [feat] 결제 요청 페이지 조회 데이터에 오너 프로필 이미지, 시작/종료일 추가

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/service/dto/RequestRemittanceResult.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/rental/service/dto/RequestRemittanceResult.java
@@ -3,6 +3,7 @@ package com.nextdoor.nextdoor.domain.rental.service.dto;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -12,8 +13,11 @@ import java.math.BigDecimal;
 public class RequestRemittanceResult {
 
     private String ownerNickname;
+    private String ownerProfileImageUrl;
     private BigDecimal finalAmount;
     private BigDecimal deposit;
     private String accountNo;
     private String bankCode;
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
