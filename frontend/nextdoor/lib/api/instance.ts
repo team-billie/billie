f65f77f4f4a2870593d030fc87570c0f32169d23
Choijake <shameless8@naver.com>
2025-05-19 11:45:35 +0900

    :sparkles: [feat] 실시간 입력 처리 지연 방지지

--- frontend/nextdoor/lib/api/instance.ts
+++ frontend/nextdoor/lib/api/instance.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "http://k12e205.p.ssafy.io:8081",
   headers: {
     "Content-Type": "application/json",
   },
