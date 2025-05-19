7220137f465d3fcd847f606c118ec3bbf30edec7
허준수 <gjwnstn515@naver.com>
2025-05-20 06:06:50 +0900

    :sparkles: [feat] 쿼리포트 - 어뎁터 수정 #S12P31E205-162

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/query/ChatRentalQueryAdapter.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/query/ChatRentalQueryAdapter.java
@@ -1,32 +1,32 @@
-//package com.nextdoor.nextdoor.query;
-//
-//import com.nextdoor.nextdoor.common.Adapter;
-//import com.nextdoor.nextdoor.domain.chat.application.dto.RentalDto;
-//import com.nextdoor.nextdoor.domain.chat.port.ChatRentalQueryPort;
-//import com.nextdoor.nextdoor.domain.rental.domain.QRental;
-//import com.querydsl.core.types.Projections;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
-//import lombok.RequiredArgsConstructor;
-//
-//import java.util.Optional;
-//
-//@RequiredArgsConstructor
-//@Adapter
-//public class ChatRentalQueryAdapter implements ChatRentalQueryPort {
-//
-//    private final JPAQueryFactory jpaQueryFactory;
-//    private final QRental qRental = QRental.rental;
-//
-//    @Override
-//    public Optional<RentalDto> findById(Long rentalId) {
-//        return Optional.ofNullable(jpaQueryFactory.select(
-//                        Projections.constructor(
-//                                RentalDto.class,
-//                                qRental.rentalId,
-//                                qRental.rentalProcess
-//                        ))
-//                .from(qRental)
-//                .where(qRental.rentalId.eq(rentalId))
-//                .fetchOne());
-//    }
-//}
+package com.nextdoor.nextdoor.query;
+
+import com.nextdoor.nextdoor.common.Adapter;
+import com.nextdoor.nextdoor.domain.chat.dto.RentalDto;
+import com.nextdoor.nextdoor.domain.chat.port.ChatRentalQueryPort;
+import com.nextdoor.nextdoor.domain.rental.domain.QRental;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Adapter
+public class ChatRentalQueryAdapter implements ChatRentalQueryPort {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QRental qRental = QRental.rental;
+
+    @Override
+    public Optional<RentalDto> findById(Long rentalId) {
+        return Optional.ofNullable(jpaQueryFactory.select(
+                        Projections.constructor(
+                                RentalDto.class,
+                                qRental.rentalId,
+                                qRental.rentalProcess
+                        ))
+                .from(qRental)
+                .where(qRental.rentalId.eq(rentalId))
+                .fetchOne());
+    }
+}
