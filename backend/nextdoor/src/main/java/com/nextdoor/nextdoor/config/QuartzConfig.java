929cbd43a053d4f4220df566fcaaf150c0083103
Choijake <shameless8@naver.com>
2025-05-21 03:36:51 +0900

    :bug: [fix] RentalEndJob에 Component 어노테이션 추가

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/QuartzConfig.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/QuartzConfig.java
@@ -1,9 +1,48 @@
 package com.nextdoor.nextdoor.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
 @Configuration
 @EnableScheduling
 public class QuartzConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public SpringBeanJobFactory springBeanJobFactory() {
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setJobFactory(springBeanJobFactory());
+        return schedulerFactory;
+    }
+
+    public class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory {
+
+        private AutowireCapableBeanFactory beanFactory;
+
+        @Override
+        protected Object createJobInstance(org.quartz.spi.TriggerFiredBundle bundle) throws Exception {
+            final Object job = super.createJobInstance(bundle);
+            beanFactory.autowireBean(job);
+            return job;
+        }
+
+        public void setApplicationContext(ApplicationContext applicationContext) {
+            this.beanFactory = applicationContext.getAutowireCapableBeanFactory();
+        }
+    }
 }
